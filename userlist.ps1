aa853c34e7907f1dd9a9fb673e3b0273b939376e
Kaj Schmidt <kaj.schmidt@ga.ntig.se>
2023-10-12 14:52:21 +0200

    usernames

---
+++ userlist.ps1
@@ -0,0 +1,15 @@
+$users = Import-Csv userlist.csv | select *,username,mail,password
+$characters = "abcdefghojklmnop1234567ABCDEFGHIJKL!&"
+
+foreach($user in $users) {
+    $user.username = $user.firstname + "." + $user.lastname
+    $user.mail = $user.firstname + "." + $user.lastname + "@gmail.com"
+    $password = ""
+    for ($i = 0; $i -lt 8; $i++) {
+        $randomnum = Get-Random -max $characters.Length
+        $password += $characters[$randomnum]
+    }
+    $user.password = $password
+}
+
+$users
