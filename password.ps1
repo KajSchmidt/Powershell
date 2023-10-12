6288e1f8708b82635acbc95396e6068f9af49713
Kaj Schmidt <kaj.schmidt@ga.ntig.se>
2023-10-12 12:46:20 +0200

    password

--- password.ps1
+++ password.ps1
@@ -1,6 +1,17 @@
 $characters = "abcdefghojklmnop1234567ABCDEFGHIJKL!&"
 $password = ""
 
+$randomnum = Get-Random -max 16
+$password += $characters[$randomnum]
+
+$randomnum = Get-Random -min 16 -max 23
+$password += $characters[$randomnum]
+
+$randomnum = Get-Random -min 23 -max 35
+$password += $characters[$randomnum]
+
+$randomnum = Get-Random -min 35 -max $characters.Length
+$password += $characters[$randomnum]
 
 for ($i = 0; $i -lt 8; $i++) {
     $randomnum = Get-Random -max $characters.Length
